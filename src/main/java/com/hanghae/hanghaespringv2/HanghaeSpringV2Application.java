c58df86dbccb05495eb73bb47c7e2097e3ca8928
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-28 09:44:23 +0900

    Initial commit

---
+++ src/main/java/com/hanghae/hanghaespringv2/HanghaeSpringV2Application.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaespringv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanghaeSpringV2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanghaeSpringV2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanghae/hanghaespringv2/HanghaeSpringV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanghae.hanghaespringv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HanghaeSpringV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
