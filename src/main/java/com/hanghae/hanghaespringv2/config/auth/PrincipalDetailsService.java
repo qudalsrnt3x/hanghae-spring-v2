f9777b017ee1febd7b5edc1a70b7ce40bdbd17f8
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-31 18:08:13 +0900

    [be] feat: 로그인 기능 추가

---
+++ src/main/java/com/hanghae/hanghaespringv2/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,27 @@
+package com.hanghae.hanghaespringv2.config.auth;
+
+import com.hanghae.hanghaespringv2.model.user.User;
+import com.hanghae.hanghaespringv2.model.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+@Configuration
+public class PrincipalDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User principal = userRepository.findByUsername(username).orElseThrow(
+                () -> new UsernameNotFoundException("해당 유저를 찾을 수 없습니다.")
+        );
+
+        // 세션에 유저 정보 저장
+        return new PrincipalDetails(principal);
+    }
+}
--- src/main/java/com/hanghae/hanghaespringv2/model/user/UserRepository.java
+++ src/main/java/com/hanghae/hanghaespringv2/model/user/UserRepository.java
@@ -2,5 +2,9 @@ package com.hanghae.hanghaespringv2.model.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
 }
