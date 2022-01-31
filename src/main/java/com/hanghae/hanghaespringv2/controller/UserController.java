a79f9b1344b570965fcc90c4a14084426fa20595
qudalsrnt3x <qudalsrnt3x@gmail.com>
2022-01-31 16:15:36 +0900

    [fe] feat: 메인 화면, 로그인 화면 구현

---
+++ src/main/java/com/hanghae/hanghaespringv2/controller/UserController.java
@@ -0,0 +1,19 @@
+package com.hanghae.hanghaespringv2.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+
+    @GetMapping("/auth/join")
+    public String joinForm() {
+        return "user/joinForm";
+    }
+
+    @GetMapping("/auth/login")
+    public String loginForm() {
+        return "user/loginForm";
+    }
+
+}
